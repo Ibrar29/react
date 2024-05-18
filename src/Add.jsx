0257651e545ab29467444a9d7fe17d92a1c816b5
Ibrar29 <156094384+Ibrar29@users.noreply.github.com>
2024-05-18 00:23:44 -0700

    first commit

---
+++ src/Add.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+export default function Add() {
+  return (
+    <>
+      <div>
+        <input type="button" value="click" />
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eveniet enim
+          maxime perspiciatis hic praesentium fuga sequi vero. Ratione illo
+          velit, iste, corporis quibusdam commodi, ipsam recusandae omnis
+          similique vitae dolo
+        </p>
+        <ul>
+            <li>ibrar</li>
+
+        </ul>
+      </div>
+    </>
+  );
+}
---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Hello from './Hello'
+import Add from './Add'
+
+export default function App() {
+  return (<>
+    <div>
+      <h1>Sheikh</h1>
+    </div>
+    <div>
+      <Hello/>
+      <Add />
+    </div>
+    </>
+  )
+
+}
+
+
